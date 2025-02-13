b9221264ff3afef741e424fd47c13da3854134eb
PonizhenDoRyadovogo <art25ivan@gmail.com>
2025-02-14 01:02:43 +0700

    Изменил логику функции только под полином третьей степени

--- lobachevsky.m
+++ lobachevsky.m
@@ -1,38 +1,27 @@
-function roots = lobachevckyMethod(poly, degree_accuracy)
+function [x1, x2, x3] = lobachevckyMethod(poly, degree_accuracy)
     coeffs_vector = coeffs(poly, 'ALL');
-    degree_poly = length(coeffs_vector) - 1;
-    quadr_matrix = zeros(degree_accuracy, length(coeffs_vector));
-    roots = zeros(1, degree_poly);
+    quadr_matrix = zeros(degree_accuracy + 1, length(coeffs_vector));
+    
     %заполнили первую строку в матрице квадрирования
     for column = 1:length(coeffs_vector)
         quadr_matrix(1,column) = coeffs_vector(column);
     end
     %заполняем матрцу квадрирования
-    for row = 2:degree_accuracy % отдельно обрабатываем крайние коэффициенты
+    for row = 2:(degree_accuracy + 1) % отдельно обрабатываем крайние коэффициенты
         quadr_matrix(row, 1) = quadr_matrix(row - 1, 1) * quadr_matrix(row - 1, 1);
-        quadr_matrix(row, degree_poly + 1) = quadr_matrix(row - 1, degree_poly + 1) * quadr_matrix(row - 1, degree_poly + 1);
+        quadr_matrix(row, 4) = quadr_matrix(row - 1, 4) * quadr_matrix(row - 1, 4);
     end
-    for row = 2:degree_accuracy
-        for column = 2:degree_poly
-            a = 0;
-            k = 1;
-            while((column - k >= 0) && (column + k)<= degree_poly)
-                k = k + 1;
-            end
-            k = k - 1;
-            for p = 1:k
-                a = a + ((-1) ^ k) * 2 * quadr_matrix(row - 1, column - p) * quadr_matrix(row - 1, column + p);
-            end
-            a = (quadr_matrix(row - 1, column) * quadr_matrix(row - 1, column)) + a;
-            quadr_matrix(row, column) = a;
+    for row = 2:(degree_accuracy + 1)
+        for column = 2:3
+            quadr_matrix(row, column) = quadr_matrix(row - 1, column) * quadr_matrix(row - 1, column) - 2 * quadr_matrix(row - 1, column - 1) * quadr_matrix(row - 1, column + 1);
         end
     end
     %просматриваем матрицу квадрирования, чтобы понять какие у корни у
     %нашего уравнения
     numbers_columns = [];
     is_complex = 0; 
-    for column = 2:degree_poly
-        for row = 2:degree_accuracy
+    for column = 2:3
+        for row = 2:(degree_accuracy + 1)
             if(is_complex == 1)
                 is_complex = 0;
                 break;
@@ -46,17 +35,33 @@ function roots = lobachevckyMethod(poly, degree_accuracy)
     end
     % вычисляем корни
     m = 2 ^ (degree_accuracy);
-    for k = 1:degree_poly
-        if(ismember(k, numbers_columns)) % комплексно-сопряженные корни
-            r = (quadr_matrix(degree_accuracy, 4)/quadr_matrix(degree_accuracy, 1)) ^ (1/(2 * m));
-            %roots(1)
-        else % только вещественные корни
-            roots(k) = (abs((quadr_matrix(degree_accuracy, k))/(quadr_matrix(degree_accuracy, k - 1)))) ^ (1/m);
-            if(subs(poly, roots(k)) > subs(poly, -roots(k)))
-                roots(k) = -roots(k);
-            end
+    if(isempty(numbers_columns))
+        x1 = (abs((quadr_matrix(degree_accuracy + 1, 2))/(quadr_matrix(degree_accuracy + 1, 1)))) ^ (1/m);
+        if(abs(subs(poly, x1)) > abs(subs(poly, -x1)))
+            x1 = -x1;
         end
+        x2 = (abs((quadr_matrix(degree_accuracy + 1, 3))/(quadr_matrix(degree_accuracy + 1, 2)))) ^ (1/m);
+        if(abs(subs(poly, x2)) > abs(subs(poly, -x2)))
+            x2 = -x2;
+        end
+        x3 = (abs((quadr_matrix(degree_accuracy + 1, 4))/(quadr_matrix(degree_accuracy + 1, 3)))) ^ (1/m);
+        if(abs(subs(poly, x3)) > abs(subs(poly, -x3)))
+            x3 = -x3;
+        end
+    else
+        
     end
+    % for k = 1:3
+    %     if(ismember(k, numbers_columns)) % комплексно-сопряженные корни
+    %         r = (quadr_matrix(degree_accuracy, 4)/quadr_matrix(degree_accuracy, 1)) ^ (1/(2 * m));
+    %         %roots(1)
+    %     else % только вещественные корни
+    %         x1 = (abs((quadr_matrix(degree_accuracy, k))/(quadr_matrix(degree_accuracy, k - 1)))) ^ (1/m);
+    %         if(subs(poly, roots(k)) > subs(poly, -roots(k)))
+    %             roots(k) = -roots(k);
+    %         end
+    %     end
+    % end
 end
 
 flag = 1;
